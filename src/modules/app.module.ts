0e023619aef68f24a490f0371a698be6b9baf20b
ruben <ruben.letterbox@gmail.com>
2023-12-19 19:52:35 +0400

    mus-2: add modules

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -3,14 +3,24 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { DataSource } from "typeorm";
 import { ConfigModule } from "@nestjs/config";
 
+import { AdminsModule } from "./admins/admins.module";
 import { AuthModule } from "./auth/auth.module";
+import { ClientsModule } from "./clients/clients.module";
+import { ContainersModule } from "./containers/containers.module";
 import { CoreModule } from "./core-module/core.module";
+import { DriversModule } from "./drivers/drivers.module";
+import { EmployeesModule } from "./employees/employees.module";
+import { GatheringModule } from "src/modules/gathering/gathering.module";
+import { OffloadsModule } from "./offloads/offloads.module";
+import { ProductsModule } from "./products/products.module";
+import { WheelbarrowsModule } from "./wheelbarrows/wheelbarrows.module";
+import { WorkdaysModule } from "./workdays/workdays.module";
 
+import { ClientsEntity } from "./clients/clients.entity";
 import { UsersEntity } from "./core-module/users/users.entity";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { AdminsModule } from "./admins/admins.module";
 
 @Module({
   imports: [
@@ -27,11 +37,20 @@ import { AdminsModule } from "./admins/admins.module";
       database: process.env.MYSQL_DATABASE,
       synchronize: true, //NOTE: shouldn't be used in production - otherwise you can lose production data.
       autoLoadEntities: true,
-      entities: [UsersEntity],
+      entities: [ClientsEntity, UsersEntity],
     }),
     CoreModule,
     AuthModule,
     AdminsModule,
+    ClientsModule,
+    ContainersModule,
+    DriversModule,
+    EmployeesModule,
+    GatheringModule,
+    OffloadsModule,
+    ProductsModule,
+    WheelbarrowsModule,
+    WorkdaysModule,
   ],
   controllers: [AppController],
   providers: [AppService],
