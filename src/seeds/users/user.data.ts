3df8fbf9d6fc9d4329009f248fff22c1ee93428e
ruben <ruben.letterbox@gmail.com>
2023-12-27 20:06:53 +0400

    mus-155: use env superadmin seed variables

--- src/seeds/users/user.data.ts
+++ src/seeds/users/user.data.ts
@@ -4,11 +4,11 @@ import { UsersEntity } from "src/modules/core-module/users/users.entity";
 
 export const userList: Partial<UsersEntity>[] = [
   {
-    firstName: "Jason",
-    lastName: "Statham",
-    phone: "+38123456789",
-    email: "superadmin@dmail.com",
-    password: "123Abc!_z",
+    firstName: process.env.SEED_SUPERADMIN_FIRSTNAME,
+    lastName: process.env.SEED_SUPERADMIN_LASTNAME,
+    phone: process.env.SEED_SUPERADMIN_PHONE,
+    email: process.env.SEED_SUPERADMIN_EMAIL,
+    password: process.env.SEED_SUPERADMIN_PASSWORD,
     role: ERole.SUPERADMIN,
     permissions: Object.values(EPermission),
   },
