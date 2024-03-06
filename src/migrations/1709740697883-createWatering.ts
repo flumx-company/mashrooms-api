fa5061fcecc4102df32b4e6944412ab2052492bc
flumx-company <117813009+flumx-company@users.noreply.github.com>
2024-03-06 23:21:17 +0300

    update migration

--- src/migrations/1709740697883-createWatering.ts
+++ src/migrations/1709740697883-createWatering.ts
@@ -4,7 +4,7 @@ export class CreateWatering1709740697883 implements MigrationInterface {
     name = 'CreateWatering1709740697883'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE \`waterings\` (\`id\` int NOT NULL AUTO_INCREMENT, \`createdAt\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`volume\` decimal(7,0) NOT NULL DEFAULT '0', \`dateTimeFrom\` date NOT NULL, \`dateTimeTo\` date NULL, \`target\` enum ('MUSHROOM', 'PEAT') NOT NULL DEFAULT 'MUSHROOM', \`hasDrug\` tinyint NULL DEFAULT 0, \`shiftId\` int NULL, \`batchId\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`CREATE TABLE \`waterings\` (\`id\` int NOT NULL AUTO_INCREMENT, \`createdAt\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`volume\` decimal(7,0) NOT NULL DEFAULT '0', \`dateTimeFrom\` datetime NOT NULL, \`dateTimeTo\` datetime NULL, \`target\` enum ('MUSHROOM', 'PEAT') NOT NULL DEFAULT 'MUSHROOM', \`hasDrug\` tinyint NULL DEFAULT 0, \`shiftId\` int NULL, \`batchId\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`ALTER TABLE \`waterings\` ADD CONSTRAINT \`FK_a6803aaece5dc2e7d3f8f6b45b9\` FOREIGN KEY (\`shiftId\`) REFERENCES \`shifts\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`waterings\` ADD CONSTRAINT \`FK_911fae0bd5227677056eb35bc75\` FOREIGN KEY (\`batchId\`) REFERENCES \`batches\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
