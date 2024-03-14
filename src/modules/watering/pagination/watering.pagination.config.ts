4cdef0643107dcb83013417d4eb67e64ba780bd7
ruben <ruben.letterbox@gmail.com>
2024-03-14 15:53:42 +0400

    mus-207: add watering filters

--- src/modules/watering/pagination/watering.pagination.config.ts
+++ src/modules/watering/pagination/watering.pagination.config.ts
@@ -1,4 +1,4 @@
-import { PaginateConfig } from 'nestjs-paginate'
+import { FilterOperator, PaginateConfig } from 'nestjs-paginate'
 
 import { Watering } from '../watering.entity'
 
@@ -7,4 +7,10 @@ export const wateringPaginationConfig: PaginateConfig<Watering> = {
   searchableColumns: ['dateTimeFrom', 'batch.dateFrom'],
   defaultSortBy: [['id', 'ASC']],
   relations: ['shift', 'batch', 'shift.employee'],
+  filterableColumns: {
+    ['batch.id']: [FilterOperator.EQ],
+    ['shift.employee.id']: [FilterOperator.EQ],
+    target: [FilterOperator.EQ],
+    dateTimeFrom: [FilterOperator.ILIKE],
+  },
 }
