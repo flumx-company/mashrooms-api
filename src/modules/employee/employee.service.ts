8d2a71be0ae021d992559604ef10016e2148b7f3
FlumX <117813009+flumx-company@users.noreply.github.com>
2024-04-24 01:32:07 +0300

    Update employee.service.ts

--- src/modules/employee/employee.service.ts
+++ src/modules/employee/employee.service.ts
@@ -26,6 +26,8 @@ export class EmployeeService {
   constructor(
     @InjectRepository(Employee)
     private employeeRepository: Repository<Employee>,
+    @InjectRepository(PublicFile)
+    private publicFileRepository: Repository<PublicFile>,
     private readonly fileUploadService: FileUploadService,
   ) {}
 
@@ -46,11 +48,24 @@ export class EmployeeService {
       .createQueryBuilder('employee')
       .leftJoinAndSelect('employee.avatars', EFileCategory.EMPLOYEE_AVATARS)
       .where('employee.id = :id', { id })
-      .leftJoinAndSelect('employee.documents', EFileCategory.EMPLOYEE_DOCUMENTS)
-      .where('employee.id = :id', { id })
-      .leftJoinAndSelect('employee.shifts', 'shifts')
-      .where('employee.id = :id', { id })
-      .getOne()
+      // .leftJoinAndSelect('employee.documents', EFileCategory.EMPLOYEE_DOCUMENTS)
+      // .where('employee.id = :id', { id })
+      // .leftJoinAndSelect('employee.shifts', 'shifts')
+      // .where('employee.id = :id', { id })
+      // .getOne()
+  }
+  
+  findEmployeeDocuments(id: number): Promise<Nullable<PublicFile>> {
+
+    const query = {
+      "filter.employeeDocuments.id": id
+    }
+    return paginate(, this.publicFileRepository, {
+      relations: [EFileCategory.EMPLOYEE_DOCUMENTS],
+      employeeDocuments: {
+        ['employeeDocuments.id']: [FilterOperator.EQ],
+      }
+    })
   }
 
   async createEmployee(
