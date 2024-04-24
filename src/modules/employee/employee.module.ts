740ebdae06a164657855c349441e6d30f6c97d86
FlumX <117813009+flumx-company@users.noreply.github.com>
2024-04-24 14:40:50 +0300

    Update employee.module.ts

--- src/modules/employee/employee.module.ts
+++ src/modules/employee/employee.module.ts
@@ -3,13 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { CoreModule } from '@mush/modules/core-module/core.module'
 import { FileUploadModule } from '@mush/modules/file-upload/file-upload.module'
-
+import { PublicFile } from '../file-upload/public-file.entity';
 import { EmployeeController } from './employee.controller'
 import { Employee } from './employee.entity'
 import { EmployeeService } from './employee.service'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Employee]), CoreModule, FileUploadModule],
+  imports: [TypeOrmModule.forFeature([Employee, PublicFile]), CoreModule, FileUploadModule],
   controllers: [EmployeeController],
   providers: [EmployeeService],
   exports: [EmployeeService],
