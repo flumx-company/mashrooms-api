58f18bb1471394fd7378a419e8ff74669e45a5ed
FlumX <117813009+flumx-company@users.noreply.github.com>
2024-05-05 20:00:43 +0300

    Update create.batch.dto.ts

--- src/modules/batch/dto/create.batch.dto.ts
+++ src/modules/batch/dto/create.batch.dto.ts
@@ -25,6 +25,45 @@ export class CreateBatchDto {
   })
   readonly chamberId: number
 
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    example: 'Peat supplier 1',
+    description: 'Enter peat supplier.',
+    type: String,
+  })
+  readonly peatSupplier: string
+
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  @ApiProperty({
+    example: 100,
+    description: 'Enter the peat weight by kg.',
+    type: Number,
+  })
+  readonly peatWeight: number
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({
+    example: '2024-01-25',
+    description: 'Enter the peat load date.',
+    type: String,
+  })
+  readonly peatLoadDate: string
+
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  @ApiProperty({
+    example: 100,
+    description: 'Enter the peat price.',
+    type: Number,
+  })
+  readonly peatPrice: number
+  
   @IsNotEmpty()
   @ApiProperty({
     example: [
