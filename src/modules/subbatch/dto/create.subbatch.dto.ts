8c88e6ea6f59463dd23d46c1dbe50bf33efbed17
FlumX <117813009+flumx-company@users.noreply.github.com>
2024-05-05 20:00:55 +0300

    Update create.subbatch.dto.ts

--- src/modules/subbatch/dto/create.subbatch.dto.ts
+++ src/modules/subbatch/dto/create.subbatch.dto.ts
@@ -63,46 +63,4 @@ export class CreateSubbatchDto {
     type: Number,
   })
   readonly compostPrice: number
-
-  @IsOptional()
-  @IsNotEmpty()
-  @IsString()
-  @ApiProperty({
-    example: 'Peat supplier 1',
-    description: 'Enter peat supplier.',
-    type: String,
-  })
-  readonly peatSupplier: string
-
-  @IsOptional()
-  @IsNotEmpty()
-  @IsNumber()
-  @Min(1)
-  @ApiProperty({
-    example: 100,
-    description: 'Enter the peat weight by kg.',
-    type: Number,
-  })
-  readonly peatWeight: number
-
-  @IsOptional()
-  @IsString()
-  @IsNotEmpty()
-  @ApiProperty({
-    example: '2024-01-25',
-    description: 'Enter the peat load date.',
-    type: String,
-  })
-  readonly peatLoadDate: string
-
-  @IsOptional()
-  @IsNotEmpty()
-  @IsNumber()
-  @Min(1)
-  @ApiProperty({
-    example: 100,
-    description: 'Enter the peat price.',
-    type: Number,
-  })
-  readonly peatPrice: number
 }
