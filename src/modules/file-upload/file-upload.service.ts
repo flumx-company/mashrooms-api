c031599de479b56965777a34d13bb7a7c3ee5d55
danil <d.utyuzh@codeska.com>
2025-11-25 21:43:18 +0200

    develop-1 - fixing minio bucket strings

--- src/modules/file-upload/file-upload.service.ts
+++ src/modules/file-upload/file-upload.service.ts
@@ -3,7 +3,7 @@ import { randomUUID } from 'crypto'
 import * as stream from 'stream'
 import { Repository } from 'typeorm'
 
-import { HttpException, HttpStatus, Injectable, OnModuleInit, Logger } from '@nestjs/common'
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { InjectRepository } from '@nestjs/typeorm'
 
@@ -14,23 +14,13 @@ import { BufferedFile } from './file.model'
 import { PublicFile } from './public-file.entity'
 
 @Injectable()
-export class FileUploadService implements OnModuleInit {
+export class FileUploadService {
   private s3: S3
-  private readonly logger = new Logger(FileUploadService.name)
-  private readonly bucketName: string
-
   constructor(
     @InjectRepository(PublicFile)
     private publicFilesRepository: Repository<PublicFile>,
     private readonly configService: ConfigService,
   ) {
-    this.bucketName = this.configService.get('MINIO_BUCKET')
-    
-    if (!this.bucketName) {
-      this.logger.error('MINIO_BUCKET is not configured in environment variables')
-      throw new Error('MINIO_BUCKET environment variable is required')
-    }
-    
     this.s3 = new S3({
       accessKeyId: this.configService.get('MINIO_ACCESS_KEY'),
       secretAccessKey: this.configService.get('MINIO_SECRET_KEY'),
@@ -43,48 +33,11 @@ export class FileUploadService implements OnModuleInit {
     })
   }
 
-  async onModuleInit() {
-    await this.ensureBucketExists()
-  }
-
-  private async ensureBucketExists(): Promise<void> {
-    try {
-      // Проверяем, существует ли bucket
-      await this.s3.headBucket({ Bucket: this.bucketName }).promise()
-      this.logger.log(`Bucket "${this.bucketName}" already exists`)
-    } catch (error) {
-      if (error.statusCode === 404 || error.code === 'NotFound' || error.code === 'NoSuchBucket') {
-        // Bucket не существует, создаем его
-        // Для MinIO не требуется LocationConstraint
-        try {
-          await this.s3
-            .createBucket({
-              Bucket: this.bucketName,
-            })
-            .promise()
-          this.logger.log(`Bucket "${this.bucketName}" created successfully`)
-        } catch (createError) {
-          // Если bucket уже был создан другим процессом, это нормально
-          if (createError.code === 'BucketAlreadyOwnedByYou' || createError.code === 'BucketAlreadyExists') {
-            this.logger.log(`Bucket "${this.bucketName}" already exists (created by another process)`)
-          } else {
-            this.logger.error(`Failed to create bucket "${this.bucketName}":`, createError)
-            throw createError
-          }
-        }
-      } else {
-        // Другая ошибка (например, нет доступа)
-        this.logger.error(`Error checking bucket "${this.bucketName}":`, error)
-        throw error
-      }
-    }
-  }
-
   private async removeS3file(fileInfo) {
     await Promise.all([
       this.s3
         .deleteObject({
-          Bucket: this.bucketName,
+          Bucket: this.configService.get('MINIO_BUCKET'),
           Key: fileInfo.key,
         })
         .promise(),
@@ -93,13 +46,10 @@ export class FileUploadService implements OnModuleInit {
   }
 
   async uploadPublicFile(file: BufferedFile): Promise<PublicFile> {
-    // Убеждаемся, что bucket существует перед загрузкой
-    await this.ensureBucketExists()
-    
     const { buffer, fieldname, originalname, mimetype } = file
     const uploadResult = await this.s3
       .upload({
-        Bucket: this.bucketName,
+        Bucket: this.configService.get('MINIO_BUCKET'),
         Body: buffer,
         Key: `${fieldname}/${randomUUID()}-${originalname}`,
       })
@@ -118,14 +68,11 @@ export class FileUploadService implements OnModuleInit {
   }
 
   async uploadPublicFiles(files: BufferedFile[]): Promise<PublicFile[]> {
-    // Убеждаемся, что bucket существует перед загрузкой
-    await this.ensureBucketExists()
-    
     const uploadResultList = await Promise.all(
       files.map(({ buffer, fieldname, originalname }) => {
         return this.s3
           .upload({
-            Bucket: this.bucketName,
+            Bucket: this.configService.get('MINIO_BUCKET'),
             Body: buffer,
             Key: `${fieldname}/${randomUUID()}-${originalname}`,
           })
@@ -208,7 +155,7 @@ export class FileUploadService implements OnModuleInit {
     if (fileInfo) {
       const stream = this.s3
         .getObject({
-          Bucket: this.bucketName,
+          Bucket: this.configService.get('MINIO_BUCKET'),
           Key: fileInfo.key,
         })
         .createReadStream()
